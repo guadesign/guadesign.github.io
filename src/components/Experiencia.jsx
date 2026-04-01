ed1588dae523c4a1b8fc5b10ded9659aa8e1979c
guadesign <guadie.design@gmail.com>
2026-04-01 11:42:32 -0300

    Update Experiencia.jsx

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { experiencia } from '../data/content'
-import InstagramEmbed from './InstagramEmbed' // Importamos el componente que creamos
+import InstagramEmbed from './InstagramEmbed' 
 import styles from './Experiencia.module.css'
 
 export default function Experiencia() {
@@ -11,15 +11,16 @@ export default function Experiencia() {
       
       <div className={styles.timeline}>
         {experiencia.map(exp => (
-          <div key={exp.id} className={styles.item}>
+          /* Usamos .block para que tome el borde amarillo y el margen de tu CSS */
+          <div key={exp.id} className={styles.block}>
             <div className={styles.header}>
               <span className={styles.periodo}>{exp.periodo}</span>
-              <h3 className={styles.empresa}>{exp.nombre}</h3>
+              <h3 className={styles.nombre}>{exp.nombre}</h3>
               <span className={styles.rol}>{exp.rol}</span>
             </div>
             <p className={styles.desc}>{exp.desc}</p>
             
-            {/* Renderizamos los posteos de Instagram si existen */}
+            {/* Si la experiencia tiene posteos en content.js, los muestra acá */}
             {exp.posts && exp.posts.length > 0 && (
               <div className={styles.instagramGrid}>
                 {exp.posts.map((postUrl, index) => (
