8c4509acead689b3f7302be253ade3f9a2c444c9
guadesign <guadie.design@gmail.com>
2026-04-09 13:21:47 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Nav from './components/Nav'
 import Hero from './components/Hero'
-import Proyectos from './components/Proyectos'
 import Sobre from './components/Sobre'
+import Proyectos from './components/Proyectos'
 import Experiencia from './components/Experiencia'
 import Contacto from './components/Contacto'
 import './index.css'
@@ -14,7 +14,7 @@ function App() {
       <main>
         <Hero />
         <Sobre />
-        <Proyectos />
+        <Proyectos lang="es" />
         <Experiencia />
       </main>
       <Contacto />
