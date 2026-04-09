f5c8536c665e074bc0b0a673cf83cec6361cd346
guadesign <guadie.design@gmail.com>
2026-04-09 10:09:02 -0300

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { labels } from '../data/content'
 import styles from './Nav.module.css'
 
-export default function Nav({ lang }) {
+export default function Nav() {
   return (
     <nav className={styles.nav}>
-      <a href="#proyectos">{labels.navProyectos[lang]}</a>
-      <a href="#experiencia">{labels.navExperiencia[lang]}</a>
-      <a href="#skills">{labels.navSkills[lang]}</a>
+      <a href="#proyectos">Proyectos</a>
+      <a href="#experiencia">Trayectoria</a>
+      <a href="#skills">Habilidades</a>
     </nav>
   )
 }
